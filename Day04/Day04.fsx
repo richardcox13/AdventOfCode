43f1d41b6fa483ac771a7fec3636db8f330d7492
Richard Cox <richard@rjcox.co.uk>
2022-12-06 19:37:29 +0000

    Day04: Solution...

---
+++ Day04/Day04.fsx
@@ -0,0 +1,28 @@
+#time "on"
+
+open System.Diagnostics;
+open System.IO
+open System.Text.RegularExpressions
+
+let input = [|
+    "2-4,6-8";
+    "2-3,4-5";
+    "5-7,7-9";
+    "2-8,3-7";
+    "6-6,4-6";
+    "2-6,4-8";
+|]
+
+let matcher = Regex("(?<a>\d+)-(?<b>\d+),(?<c>\d+)-(?<d>\d+)")
+
+let res = File.ReadLines("./Day04.txt")
+                    |> Seq.map (fun inp -> 
+                                    let m = matcher.Match(inp)
+                                    (int m.Groups["a"].Value, int m.Groups["b"].Value, int m.Groups["c"].Value, int m.Groups["d"].Value)
+                               )
+                    |> Seq.filter (fun (a, b, c, d) ->
+                                        (a <= c && d <= b)
+                                        || (c <= a && b <= d)
+                                  )
+                    |> Seq.length
+printfn "Count = %A" res
