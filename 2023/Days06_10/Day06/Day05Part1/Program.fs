058e6f69a2d806e5ecd6ccc22833a782e6c1fb13
Richard Cox <richard@rjcox.co.uk>
2023-12-06 14:59:25 +0000

    D6Pt1: proper start up, diagnostics, & parse input

--- 2023/Days06_10/Day06/Day05Part1/Program.fs
+++ 2023/Days06_10/Day06/Day05Part1/Program.fs
@@ -1,2 +1,35 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+
+type Race = {
+        Time: int
+        DistanceRecord: int
+    }
+
+let readInput filename =
+    let parseOneLine (line: string) =
+        let nosStr = ((line.Split(':'))[1]).Trim()
+        nosStr.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                        |> Seq.map(fun s -> Int32.Parse(s))
+    let content = System.IO.File.ReadAllLines(filename)
+    let times = parseOneLine content[0]
+    let records = parseOneLine content[1]
+
+    (times, records) ||>Seq.map2 (fun t r -> { Time = t; DistanceRecord = r })
+
+[<EntryPoint>]
+let main(args) =
+    printfn $"Working folder: {Environment.CurrentDirectory}"
+    let filename = args[0]
+    printfn $"Reading from {filename}"
+
+    let sw = System.Diagnostics.Stopwatch.StartNew ()
+
+    let races = readInput filename
+    printf $"There are {races |> Seq.length} races: "
+    printfn "%s" (races |> Seq.map (fun r -> $"(Time:{r.Time},Record:{r.DistanceRecord})") |> String.concat ", ")
+
+    sw.Stop()
+    let ts = sw.Elapsed.ToString("h':'mm':'ss'.'FFF")
+    printfn $"Completed in +{ts}"
+    0
