180b6eb4eafc852bad7070b25fd7666e89c74f3a
Richard Cox <richard@rjcox.co.uk>
2023-12-07 08:53:53 +0000

    D7Pt1: project entry point, timing, debug settings
    And fix paths

---
+++ 2023/Days06_10/Day07/Day07Part1/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+[<EntryPoint>]
+let main(args) =
+    printfn $"Working folder: {Environment.CurrentDirectory}"
+    let filename = args[0]
+    printfn $"Reading from {filename}"
+
+    let sw = System.Diagnostics.Stopwatch.StartNew ()
+
+
+    let result = -1
+
+    sw.Stop()
+    printfn ""
+    printfn $"Result = {result}"
+    printfn ""
+    let ts = sw.Elapsed.ToString("h':'mm':'ss'.'FFF")
+    printfn $"Completed in +{ts}"
+    printfn $"GC counts 0: {GC.CollectionCount(0)}; 1: {GC.CollectionCount(1)}; 2: {GC.CollectionCount(2)}; "
+    0
