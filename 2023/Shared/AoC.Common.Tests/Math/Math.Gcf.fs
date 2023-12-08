3d37cf79e951f826ee40eedfb81f47b03a9e36b0
Richard Cox <richard@rjcox.co.uk>
2023-12-08 20:20:01 +0000

    GCD/GCF in tests, and improved (no mutavble) implementation

--- 2023/Shared/AoC.Common.Tests/Math/Math.Gcf.fs
+++ 2023/Shared/AoC.Common.Tests/Math/Math.Gcf.fs
@@ -4,7 +4,28 @@ open AoC.Common
 open Xunit
 
 [<Fact>]
-let ``GCF 9L 12L is 3L`` () =
-    let res = Math.gcf 9L 12L
+let ``GCD 9L 12L is 3L`` () =
+    let res = Math.gcd 9L 12L
     Assert.Equal(3L, res)
 
+
+[<Fact>]
+let ``GCD 7L 2L is 1L`` () =
+    let res = Math.gcd 7L 3L
+    Assert.Equal(1L, res)
+
+
+[<Fact>]
+let ``GCD 28L 7L is 7L`` () =
+    let res = Math.gcd 28L 7L
+    Assert.Equal(7L, res)
+
+[<Fact>]
+let ``GCD 60L 25L is 5L`` () =
+    let res = Math.gcd 60L 5L
+    Assert.Equal(5L, res)
+
+[<Fact>]
+let ``GCF is the same as GCD`` () =
+    let res = Math.gcf 60L 5L
+    Assert.Equal(5L, res)
--- 2023/Shared/AoC.Common/Math.fs
+++ 2023/Shared/AoC.Common/Math.fs
@@ -1,15 +1,26 @@
 ﻿module AoC.Common.Math
 
-// Greatest Common Factor
-let gcf left right=
-    // Euclid's algorithm.
-    // TODO refactor to recursive to avoid the mutation
-    let mutable a = left
-    let mutable b = right
-    while b <> 0L do
-        let t = b
-        b <- a % b
-        a <- t
-    a
+/// Greatest Common Denominator
+let gcd left right =
+    // Euclid's algorithm.... recursive version
+    let rec inner (l: int64) r =
+        if r = 0L then
+            l
+        else
+            inner r (l % r)
+    inner left right
 
+//let gcd left right=
+//    // TODO refactor to recursive to avoid the mutation
+//    let mutable a = left
+//    let mutable b = right
+//    while b <> 0L do
+//        let t = b
+//        b <- a % b
+//        a <- t
+//    a
+
+/// Geatest Common Factor: Alias for <c>gcd</c>
+let gcf left right
+    = gcd left right
 
