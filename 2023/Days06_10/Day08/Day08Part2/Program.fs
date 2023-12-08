8ff237acc22cb11dd13f5d2844e4b0178399d722
Richard Cox <richard@rjcox.co.uk>
2023-12-08 13:06:23 +0000

    D8Pt2: do each path then calc their lead common multiplier for fast solution!

--- 2023/Days06_10/Day08/Day08Part2/Program.fs
+++ 2023/Days06_10/Day08/Day08Part2/Program.fs
@@ -31,6 +31,21 @@ let takeWHileInclusive predicate (input: 't seq) =
                 finished <- true
     }
 
+// Greatest Common Factor
+let gcf left right=
+    // Euclid's algorithm.
+    // TODO refactor to recursive to avoid the mutation
+    let mutable a = left
+    let mutable b = right
+    while b <> 0L do
+        let t = b
+        b <- a % b
+        a <- t
+    a
+
+// Least Common Multiple
+let lcm left right = (left * right) / (gcf left right)
+
 [<EntryPoint>]
 let main(args) =
     printfn $"Working folder: {Environment.CurrentDirectory}"
@@ -52,31 +67,29 @@ let main(args) =
         = map.Keys
           |> Seq.where (fun k -> k.EndsWith("A"))
           |> Seq.toArray
-    printfn $"Starting at {startingPoints}"
-    let path
-        = directions
-          |> Seq.scan (fun positions dir ->
-                                positions
-                                    |> Seq.map (fun pos ->
-                                        let opts = map[pos]
-                                        match dir with
-                                        | 'L' -> fst opts
-                                        | 'R' -> snd opts
-                                        | _ -> failwith $"Unexpected directtion '{dir}'"
-                                    )
-                                    |> Seq.toArray
-                             ) startingPoints
-           |> Seq.mapi (fun idx pos -> (idx, pos))
-           |> Seq.map (fun (idx, positions) ->
-                                    printfn $"#{idx}: %A{positions}"
-                                    (idx, positions)
-                              )
-           |> takeWHileInclusive (fun (_,positions) -> not (positions |> Seq.forall (fun p -> p.EndsWith("Z"))))
-           |> Seq.toArray
-    
-    let count = fst (path |> Array.last)
+    printfn $"Starts %A{startingPoints}"
+    let count
+        = startingPoints
+          |> Seq.map (fun start ->
+                let path
+                    = directions
+                        |> Seq.scan (fun position dir ->
+                                    let opts = map[position]
+                                    match dir with
+                                    | 'L' -> fst opts
+                                    | 'R' -> snd opts
+                                    | _ -> failwith $"Unexpected directtion '{dir}'"
+                                 ) start
+                        |> Seq.mapi (fun idx pos -> (idx, pos))
+                        |> takeWHileInclusive (fun (_,pos) -> not (pos.EndsWith("Z")))
+                        |> Seq.last
+                let pathCount = fst path
+                printfn $"Start {start}: {pathCount} steps"
+                pathCount
+          )
+          |> Seq.fold (fun a b -> lcm a b) 1
 
-    printfn $"Count = {count:``#,#``} ({count})"
+    printfn $"Final count = {count:``#,#``} ({count})"
     printfn ""
     let ts = sw.Elapsed.ToString("h':'mm':'ss'.'FFF")
     printfn $"Completed in +{ts}"
