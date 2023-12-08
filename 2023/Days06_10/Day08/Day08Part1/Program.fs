e0d8ca211a0210efc12e0b4951d73588d2b4de8b
Richard Cox <richard@rjcox.co.uk>
2023-12-08 08:57:41 +0000

    D8Pt1: create project, add to sln, starting, & entry point

---
+++ 2023/Days06_10/Day08/Day08Part1/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+[<EntryPoint>]
+let main(args) =
+    printfn $"Working folder: {Environment.CurrentDirectory}"
+    let filename = args[0]
+    printfn $"Input file {filename}"
+    let sw = System.Diagnostics.Stopwatch.StartNew ()
+
+
+
+    printfn ""
+    let ts = sw.Elapsed.ToString("h':'mm':'ss'.'FFF")
+    printfn $"Completed in +{ts}"
+    printfn $"GC counts 0: {GC.CollectionCount(0)}; 1: {GC.CollectionCount(1)}; 2: {GC.CollectionCount(2)}; "
+    9
