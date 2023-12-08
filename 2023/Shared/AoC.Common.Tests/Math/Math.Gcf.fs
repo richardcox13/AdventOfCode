6cfa442497bfe3212a7895664721d2dad4ec2c9e
Richard Cox <richard@rjcox.co.uk>
2023-12-08 18:39:48 +0000

    Add test project, 1st member of commaon, and a test

---
+++ 2023/Shared/AoC.Common.Tests/Math/Math.Gcf.fs
@@ -0,0 +1,10 @@
+﻿module Math.Gcf
+
+open AoC.Common
+open Xunit
+
+[<Fact>]
+let ``GCF 9L 12L is 3L`` () =
+    let res = Math.gcf 9L 12L
+    Assert.Equal(3L, res)
+
---
+++ 2023/Shared/AoC.Common/Math.fs
@@ -0,0 +1,15 @@
+﻿module AoC.Common.Math
+
+// Greatest Common Factor
+let gcf left right=
+    // Euclid's algorithm.
+    // TODO refactor to recursive to avoid the mutation
+    let mutable a = left
+    let mutable b = right
+    while b <> 0L do
+        let t = b
+        b <- a % b
+        a <- t
+    a
+
+
