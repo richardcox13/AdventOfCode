5b23abe004ab32d3a9fbc0e1d52128311645a8ed
Richard Cox <richard@rjcox.co.uk>
2023-12-09 11:38:28 +0000

    D9Pt1: project set up and entry point

--- 2023/Days06_10/Day09/Day09Part1/Program.fs
+++ 2023/Days06_10/Day09/Day09Part1/Program.fs
@@ -1,2 +1,23 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+[<EntryPoint>]
+let main(args) =
+    printfn $"Working folder: {Environment.CurrentDirectory}"
+    printfn ""
+    printfn $"Day 9 Part 1"
+    printfn ""
+    let filename = args[0]
+    printfn $"Input file {filename}"
+    let inputText = System.IO.File.ReadLines(filename)
+
+    let sw = System.Diagnostics.Stopwatch.StartNew ()
+
+
+    let sum = -1
+
+    printfn $"Final count = {sum:``#,#``} ({sum})"
+    printfn ""
+    let ts = sw.Elapsed.ToString("h':'mm':'ss'.'FFF")
+    printfn $"Completed in +{ts}"
+    printfn $"GC counts 0: {GC.CollectionCount(0)}; 1: {GC.CollectionCount(1)}; 2: {GC.CollectionCount(2)}; "
+    9
