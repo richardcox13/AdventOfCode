0d0975c408a1ffa0fcccd538090af9fe90190aa3
Richard Cox <richard@rjcox.co.uk>
2023-12-10 09:47:46 +0000

    D10Pt1: Create project, launch settings, emtry point

---
+++ 2023/Days06_10/Day10/Day10Part1/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+open AoC.Common
+
+[<EntryPoint>]
+let main(args) =
+    printfn $"Working folder: {Environment.CurrentDirectory}"
+    printfn $"Day 10 Part 1"
+    printfn ""
+    let filename = args[0]
+    printfn $"Input file {filename}"
+    printfn ""
+    let inputText = System.IO.File.ReadLines(filename)
+    use diag = Utility.GetTracker ()
+
+    let dist = -1
+
+    printfn ""
+    printfn $"Final dist = {dist:``#,#``} ({dist})"
+    printfn ""
+    0
