4d47b63fa2aec5e2056c13e774010162ecebb9a4
Richard Cox <richard@rjcox.co.uk>
2023-12-11 09:50:36 +0000

    Day 11: new project, lauch settings, entry point, & common projects into sln

---
+++ 2023/Days11_15/Day11/Day11Part1/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+open AoC.Common
+
+[<EntryPoint>]
+let main(args) =
+    printfn $"Working folder: {Environment.CurrentDirectory}"
+    printfn $"Day 11 Part 1"
+    printfn ""
+    let filename = args[0]
+    printfn $"Input file {filename}"
+    printfn ""
+    let input= System.IO.File.ReadAllLines(filename)
+    use diag = Utility.GetTracker ()
+
+
+    let result = -1
+    printfn ""
+    printfn $"Internal tile count = {result:``#,0``} ({result})"
+    printfn ""
+    0
