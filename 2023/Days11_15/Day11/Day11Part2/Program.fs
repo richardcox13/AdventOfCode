56553dc00f2d8f66adfa3aaf38ee12590a09eacf
Richard Cox <richard@rjcox.co.uk>
2023-12-11 12:43:34 +0000

    D11 part 2: Complete solution
    - Change from expanding the map, to adjusting the positions
    - Calc of distances like part 1, but need int64!

--- 2023/Days11_15/Day11/Day11Part2/Program.fs
+++ 2023/Days11_15/Day11/Day11Part2/Program.fs
@@ -3,6 +3,7 @@ open AoC.Common
 open AoC.Common.Core
 
 let galaxy = '#'
+let expansionFactor = 1_000_000
 
 let printMap (map: string[]) (title : string)=
     printfn ""
@@ -11,25 +12,24 @@ let printMap (map: string[]) (title : string)=
         printfn $"\t{map[r]}"
     ()
 
-let expandRows (map: string[]) =
+let findEmptyRows (map: string[]) =
     seq {
         for r = 0 to (map.Length-1) do
             let row = map[r]
-            yield row
             if row.IndexOf(galaxy) = -1 then
-                yield row
+                yield r
     } |> Seq.toArray
 
-let expandColumns (map: string[]) =
-    let invertMap (map: string[]) =
-        Array.init (map[0].Length)
-                (fun col ->
-                                seq {
-                                    for row in 0 .. (map.Length-1) -> map[row][col]
-                                } |> String.Concat
-                           )
-
-    map |> invertMap |> expandRows |> invertMap
+let findEmptyColumns (map: string[]) =
+    seq {
+        for col = 0 to (map[0].Length-1) do
+            let colHasGalaxy
+                = seq { 0 .. (map.Length-1) }
+                  |> Seq.map (fun row -> map[row][col])
+                  |> Seq.exists (fun c -> c = galaxy)
+            if not colHasGalaxy then
+                yield col
+    } |> Seq.toArray
 
 let extractPositions targetChar (map: string[]) =
     seq {
@@ -47,6 +47,17 @@ let getAllDistinctPairs input =
                 yield input[outer], input[inner]
     }
 
+let applyExpansion expansionRows expansionColumns (galaxy: Position)=
+    let rowsToExpand = expansionRows |> Seq.where (fun r -> r < galaxy.Row) |> Seq.length
+    let colsToExpand = expansionColumns |> Seq.where (fun c -> c < galaxy.Col) |> Seq.length
+    let ef = expansionFactor - 1
+    let res = {
+        Row = galaxy.Row + (rowsToExpand * ef)
+        Col = galaxy.Col + (colsToExpand * ef)
+    }
+    printfn $"\texpanded {galaxy} to {res} with {rowsToExpand} rows and {colsToExpand} cols expanded"
+    res
+
 [<EntryPoint>]
 let main(args) =
     printfn $"Working folder: {Environment.CurrentDirectory}"
@@ -59,28 +70,34 @@ let main(args) =
     use diag = Utility.GetTracker ()
 
     printMap map "Start"
-
-    let mapExpanded = map |> expandRows |> expandColumns
-    printMap mapExpanded "Fully expanded"
     printfn ""
-    printfn $"Originally {map.Length} rows x {map[0].Length} columns; now {mapExpanded.Length} rows x {mapExpanded[0].Length} cols"
 
-    printfn ""
     let galaxies
-        = mapExpanded
+        = map
           |> extractPositions galaxy
           |> Seq.toArray
     let gg = galaxies |> Seq.map (fun g -> $"{g}") |> String.concat ", "
     printfn $"Found {galaxies.Length} galaxies: {gg}"
 
-    let galaxyPairs = galaxies |> getAllDistinctPairs |> Seq.toArray
-    printfn $"Found {galaxyPairs  |> Array.length} pairs"
+    let emptyRows = findEmptyRows map
+    let emptyCols = findEmptyColumns map
+    let sep = ", "
+    printfn $"There are {emptyRows.Length} empty rows: {emptyRows |> Array.map (fun x -> x.ToString()) |> String.concat sep}"
+    printfn $"There are {emptyCols.Length} empty columns: {emptyCols |> Array.map (fun x -> x.ToString()) |> String.concat sep}"
+
+    let expandedGalaxies
+        = galaxies
+          |> Seq.map (fun g -> applyExpansion emptyRows emptyCols g)
+          |> Seq.toArray
+    let gg = expandedGalaxies |> Seq.map (fun g -> $"{g}") |> String.concat ", "
+    printfn $"Expanded galaxies: {gg}"
+    let galaxyPairs = expandedGalaxies |> getAllDistinctPairs |> Seq.toArray
 
     let distanaces
         = galaxyPairs
           |> Seq.map (fun (left, right) ->
-                let dist = abs (left.Row - right.Row) + abs (left.Col - right.Col)
-                printfn $"\tFrom {left} to {right} dist = {dist}"
+                let dist = int64 (abs (left.Row - right.Row) + abs (left.Col - right.Col))
+                //printfn $"\tFrom {left} to {right} dist = {dist}"
                 dist
           )
     
