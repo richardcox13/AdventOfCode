b820a0decaedc4f1c74ad2d874e5addeec057532
Richard Cox <richard@rjcox.co.uk>
2023-12-12 10:33:23 +0000

    D12Pt1: combination generator over '.' & '#'
    Plus some test code

--- 2023/Days11_15/Day12/Day12Part1/Program.fs
+++ 2023/Days11_15/Day12/Day12Part1/Program.fs
@@ -1,17 +1,8 @@
 ﻿open System
 open AoC.Common
 
-[<EntryPoint>]
-let main(args) =
-    printfn $"Working folder: {Environment.CurrentDirectory}"
-    printfn $"Day 12 Part 1"
-    printfn ""
-    let filename = args[0]
-    printfn $"Input file {filename}"
-    printfn ""
-    let input = System.IO.File.ReadLines(filename)
-    use diag = Utility.GetTracker ()
 
+let showSomeInputStats (input: string seq) =
     let sizes
         = input
           |> Seq.map (fun l ->
@@ -33,6 +24,50 @@ let main(args) =
     let mostUnknowns = sizes |> Seq.map (fun (_,_,x) -> x) |> Seq.max
 
     printfn $"Longest map: {longestMap}; most groups = {mostGroups}; most inknowns = {mostUnknowns}"
+    ()
+
+let getPeriodHashCombinations len  =
+    ArgumentOutOfRangeException.ThrowIfNegativeOrZero(len)
+    let rec inner prefix len =
+        if len = 2 then
+            [| prefix+"##"; prefix+"#."; prefix+".#"; prefix+".." |] |> Seq.ofArray
+        else
+            seq {
+                let p = prefix + "#"
+                yield! inner p (len-1)
+                let p = prefix + "."
+                yield! inner p (len-1)
+            }
+
+    if len = 1 then
+        [| "#"; "." |] |> Seq.ofArray
+    else
+        inner "" len
+
+[<EntryPoint>]
+let main(args) =
+    printfn $"Working folder: {Environment.CurrentDirectory}"
+    printfn $"Day 12 Part 1"
+    printfn ""
+    let filename = args[0]
+    printfn $"Input file {filename}"
+    printfn ""
+    let input = System.IO.File.ReadLines(filename)
+    use diag = Utility.GetTracker ()
+
+    showSomeInputStats input
+
+    // Test combinations generation
+
+    let js (a: string seq) =
+        a |> Seq.map (fun s -> $"\"{s}\"") |> String.concat ", "
+
+    printfn $"Length = 1: {js (getPeriodHashCombinations 1)}"
+    printfn $"Length = 2: {js (getPeriodHashCombinations 2)}"
+    printfn $"Length = 3: {js (getPeriodHashCombinations 3)}"
+    printfn $"Length = 4: {js (getPeriodHashCombinations 4)}"
+    printfn $"Length = 5: {js (getPeriodHashCombinations 5)}"
+
 
     let result = -1
     printfn ""
