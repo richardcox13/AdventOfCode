efcb2b7efc8344bd4f6a35f82fd28b3a08ac5582
Richard Cox <richard@rjcox.co.uk>
2023-12-12 08:54:03 +0000

    D12Pt1: some stats on the full data
    Maybe brute force will be OK?

--- 2023/Days11_15/Day12/Day12Part1/Program.fs
+++ 2023/Days11_15/Day12/Day12Part1/Program.fs
@@ -12,6 +12,28 @@ let main(args) =
     let input = System.IO.File.ReadLines(filename)
     use diag = Utility.GetTracker ()
 
+    let sizes
+        = input
+          |> Seq.map (fun l ->
+                                let ss = l.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                assert (ss.Length = 2)
+                                let (map,groups) = (ss[0],ss[1])
+                                let groupCount = groups.Split(',') |> Array.length
+                                let unknownCount
+                                    = map
+                                      |> Seq.groupBy (fun x -> x)
+                                      |> Seq.where (fun (key, _) -> key = '?')
+                                      |> Seq.map (fun (_, cc) -> cc |> Seq.length)
+                                      |> Seq.head
+                                map.Length, groupCount, unknownCount
+                             )
+          |> Seq.toArray
+    let longestMap = sizes |> Seq.map (fun (x,_,_) -> x) |> Seq.max
+    let mostGroups = sizes |> Seq.map (fun (_,x,_) -> x) |> Seq.max
+    let mostUnknowns = sizes |> Seq.map (fun (_,_,x) -> x) |> Seq.max
+
+    printfn $"Longest map: {longestMap}; most groups = {mostGroups}; most inknowns = {mostUnknowns}"
+
     let result = -1
     printfn ""
     printfn $"Result = {result:``#,0``} ({result})"
