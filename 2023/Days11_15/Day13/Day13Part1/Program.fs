a7ef28ce8d552be513f9a59ff612d60521e1c3f4
Richard Cox <richard@rjcox.co.uk>
2023-12-13 09:27:04 +0000

    Day13: set up part 1 project

---
+++ 2023/Days11_15/Day13/Day13Part1/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+open AoC.Common
+
+[<EntryPoint>]
+let main(args) =
+    printfn $"Working folder: {Environment.CurrentDirectory}"
+    printfn $"Day 13 Part 1"
+    printfn ""
+    let filename = args[0]
+    printfn $"Input file {filename}"
+    printfn ""
+    let input = System.IO.File.ReadLines(filename)
+    use diag = Utility.GetTracker ()
+
+    //showSomeInputStats input
+
+    let result = -1
+
+    printfn ""
+    printfn $"Result = {result:``#,0``} ({result})"
+    printfn ""
+    0
