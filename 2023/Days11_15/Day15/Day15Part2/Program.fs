169d0ae5ba11598d2fbeb367998a7b85c85691fc
Richard Cox <richard@rjcox.co.uk>
2023-12-15 15:27:39 +0000

    Day 15: set up project for part 2 (part 1 was done interactively)

---
+++ 2023/Days11_15/Day15/Day15Part2/Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+open System.IO
+
+open AoC.Common
+
+[<EntryPoint>]
+let main(args) =
+    printfn $"Working folder: {Environment.CurrentDirectory}"
+    printfn $"Day 15 Part 2"
+    printfn ""
+    use diag = Utility.GetTracker ()
+
+    let input = if args.Length = 1 then
+                    let filename = args[0]
+                    printfn $"Input file {filename}"
+                    File.ReadAllText(filename).Trim()
+                else
+                    printfn "Using test input"
+                    "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
+    printfn ""
+
+
+    let result = -1
+    printfn ""
+    printfn $"Result = {result:``#,0``} ({result})"
+    printfn ""
+    0
