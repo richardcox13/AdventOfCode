cca249fde836f079854aebed726e3ac83f3bd2a6
Richard Cox <richard@rjcox.co.uk>
2023-12-16 09:16:17 +0000

    Day 16 Part 1: new sln (days 16-20), part 1 project

---
+++ 2023/Days16_20/Day16/Day16Part1/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+open System.Collections.Generic
+open System.IO
+open AoC.Common
+
+[<EntryPoint>]
+let main(args) =
+    printfn $"Working folder: {Environment.CurrentDirectory}"
+    printfn $"Day 16 Part 1"
+    printfn ""
+    let filename = args[0]
+    printfn $"Input file {filename}"
+    printfn ""
+    use diag = Utility.GetTracker ()
+
+    let input = System.IO.File.ReadLines(filename)
+
+
+    let result = -1
+    printfn ""
+    printfn $"Result = {result:``#,0``} ({result})"
+    printfn ""
+    0
