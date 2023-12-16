288dbf84059cf3a44e4aeb9d0552380d0e0af47a
Richard Cox <richard@rjcox.co.uk>
2023-12-16 11:38:08 +0000

    D16Pt1: main iteration started with helpers and bottoming out cases

--- 2023/Days16_20/Day16/Day16Part1/Program.fs
+++ 2023/Days16_20/Day16/Day16Part1/Program.fs
@@ -4,24 +4,24 @@ open System.IO
 open AoC.Common
 
 
-type Directions =
+type Direction =
     | Up = 1
     | Left = 2
     | Down = 3
     | Right = 4
 
-type Position = {
-        Row: int
-        Col: int
-    }
+type Position =
+    { Row: int
+      Col: int }
+    override x.ToString() = $"({x.Row}, {x.Col})"
 
 type GridCell = {
         Symbol: char
         // Already existing inbound beams
-        HasUpwardsBeam: bool
-        HasLeftwardsBeam: bool
-        HasDownwardsBeam: bool
-        HasRightwardsBeam: bool
+        mutable HasUpwardsBeam: bool
+        mutable HasLeftwardsBeam: bool
+        mutable HasDownwardsBeam: bool
+        mutable HasRightwardsBeam: bool
     }
 
 type Grid = GridCell array2d
@@ -43,6 +43,14 @@ let getCell (pos: Position) (grid: Grid) =
 
 let getCellSymbol pos grid = (getCell pos grid).Symbol
 
+let getCellDirectionFlag dir cell =
+    match dir with
+    | Direction.Up -> cell.HasUpwardsBeam
+    | Direction.Down -> cell.HasDownwardsBeam
+    | Direction.Left -> cell.HasLeftwardsBeam
+    | Direction.Right -> cell.HasRightwardsBeam
+    | _  -> failwith $"Unexpected direction {dir}"
+
 let rowCount grid = Array2D.length1 grid
 let colCount grid = Array2D.length2 grid
 
@@ -58,7 +66,54 @@ let printGrid (title:string) (grid: Grid) =
         Console.WriteLine()
     Console.WriteLine()
 
+let moveLeft pos = { Row = pos.Row; Col = pos.Col - 1 }
+let moveRight pos = { Row = pos.Row; Col = pos.Col + 1 }
+let moveUp pos = { Row = pos.Row - 1; Col = pos.Col }
+let moveDown pos = { Row = pos.Row + 1; Col = pos.Col }
 
+let applyBeams (grid: Grid) =
+    let diag (msg: string) =
+        Console.WriteLine(msg)
+        ()
+
+    let isInGrid pos =
+        pos.Row >= 0
+            && pos.Row < (rowCount grid)
+            && pos.Col >= 0
+            && pos.Col < (colCount grid)
+
+    let updateCellWithBeam pos dir =
+        let c = getCell pos grid
+        match dir with
+        | Direction.Up -> c.HasUpwardsBeam <- true
+        | Direction.Down -> c.HasDownwardsBeam <- true
+        | Direction.Left -> c.HasLeftwardsBeam <- true
+        | Direction.Right -> c.HasRightwardsBeam <- true
+        | _  -> failwith $"Unexpected direction {dir}"
+
+    // Pos is current cell, dir inbound direction
+    let rec iterate pos dir =
+        diag $"Iterate to {pos} coming {dir}"
+        if not (isInGrid pos) then
+            diag "  outside grid: end of beam"
+            ()
+        else
+            let cell = getCell pos grid
+            if getCellDirectionFlag dir cell then
+                diag "  this cell has already been visited in this direction: end of beam"
+                ()
+            else
+                updateCellWithBeam pos dir
+                let sym = getCellSymbol pos grid
+                match (dir, sym) with
+                | (x,y) -> failwith $"Can't handle direction {x} into symbol {y}"
+                ()
+
+
+    let startPos = { Row = 0; Col = 0 }
+    let startDirection = Direction.Left
+    iterate startPos startDirection
+    ()
 
 [<EntryPoint>]
 let main(args) =
@@ -73,6 +128,7 @@ let main(args) =
     let grid = buildGrid filename
     printGrid "Start" grid
 
+    applyBeams grid
 
     let result = -1
     printfn ""
