8a386c79c241425611f7f4e7a976a038ae28358a
Richard Cox <richard@rjcox.co.uk>
2023-12-17 20:26:23 +0000

    Beginning Dijkstra implementation

--- 2023/Shared/AoC.Common/Grid.fs
+++ 2023/Shared/AoC.Common/Grid.fs
@@ -5,12 +5,19 @@ type grid<'t> = 't array2d
 module Grid = 
 
     open System
+    open AoC.Common.Core
 
     let public create<'t> rows cols (value: 't) =
         ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(rows, 0)
         ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(cols, 0)
 
         Array2D.create rows cols value
+
+    let cell (position) (g: 't grid) =
+        g[position.Row, position.Col]
+
+    let cellRC row col (g: 't grid) =
+        g[row, col]
 
     let colCount (g: 't grid) =
         Array2D.length2 g
---
+++ 2023/Shared/AoC.Common/Grid_Dijkstra.fs
@@ -0,0 +1,37 @@
+﻿namespace AoC.Common
+
+module Grid = 
+
+    open System
+    open AoC.Common.Core
+    open AoC.Common.Grid
+
+    type DijkstraResultCell<'t> = {
+        Value: 't
+        Distance: int
+    }
+
+    type internal DijkstraWorkingCell<'t> = {
+        Value: 't
+        mutable Distance: int
+        mutable Unvisited: bool
+    }
+
+    let internal infinity = Int32.MaxValue
+
+    let internal copyGridForDijkstra (g: 't grid) =
+        Array2D.init (rowCount g) (colCount g)
+            (fun r c -> {
+                            Value = g[r,c]
+                            Distance = infinity
+                            Unvisited = true
+                       })
+
+    let dijkstra<'t> (getWieght: 't -> int) (start: Position) (grid: 't grid) =
+        // 1. Intialised all as unvisted, and maximum distance
+        let working = copyGridForDijkstra grid
+        // 2. Start cell gets distance of zero
+        let startCell = Grid.cell start working
+        startCell.Distance <- 0
+        startCell.Unvisited <- false
+        ()
