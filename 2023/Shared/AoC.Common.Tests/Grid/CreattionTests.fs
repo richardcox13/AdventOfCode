1489aa7a73c41d85da61bf895cb2441d893157ac
Richard Cox <richard@rjcox.co.uk>
2023-12-17 13:56:52 +0000

    Beginning Grid module (and grid type alias)

---
+++ 2023/Shared/AoC.Common.Tests/Grid/CreattionTests.fs
@@ -0,0 +1,41 @@
+﻿module AoC.Common.Tests.Grid.CreattionTests
+
+open System
+open AoC.Common
+open Xunit
+
+
+[<Fact>]
+let ``zero rows is a failure`` () =
+    //let a = Action()
+    Assert.Throws<ArgumentOutOfRangeException>(
+        (fun () -> Grid.create 0 10 "13" |> ignore)
+    ) |>ignore
+    ()
+
+[<Fact>]
+let ``zero columns is a failure`` () =
+    //let a = Action()
+    Assert.Throws<ArgumentOutOfRangeException>(
+        (fun () -> Grid.create 10 0 "13" |> ignore)
+    ) |>ignore
+    ()
+
+[<Fact>]
+let ``result has correct row count`` () =
+    let g: int Grid.Grid = Grid.create 16 5 13
+    Assert.Equal(16, Grid.rowCount g)
+    ()
+
+[<Fact>]
+let ``result has correct column count`` () =
+    let g: int grid = Grid.create 16 5 13
+    Assert.Equal(5, Grid.colCount g)
+    ()
+
+[<Fact>]
+let ``result has correct value in all cells`` () =
+    let v = "abc"
+    let g = Grid.create 16 5 v
+    Assert.Equal(16, Grid.rowCount g)
+    ()
---
+++ 2023/Shared/AoC.Common/Grid.fs
@@ -0,0 +1,23 @@
+﻿namespace AoC.Common
+
+type grid<'t> = 't array2d
+
+module Grid = 
+
+    open System
+
+
+
+    let public create<'t> rows cols (value: 't) =
+        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(rows, 0)
+        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(cols, 0)
+
+        Array2D.create rows cols value
+
+    let colCount (g: 't grid) =
+        Array2D.length2 g
+
+
+    let rowCount (g: 't grid) =
+        Array2D.length1 g
+
