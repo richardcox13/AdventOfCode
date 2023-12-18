48e95d0b0beb90c30d83860c807ea637b90be618
Richard Cox <richard@rjcox.co.uk>
2023-12-18 15:42:15 +0000

    D17Pt2: start by getting the ordered list of points (pt1 style)

--- 2023/Days16_20/Day18/Day18Part2/Program.fs
+++ 2023/Days16_20/Day18/Day18Part2/Program.fs
@@ -4,6 +4,36 @@ open System.IO
 open AoC.Common
 open AoC.Common.Core
 
+// Part 1 pasring for easier debugging...
+// Return (direction, distance)
+let parseLine (line : string)=
+    let ss = line.Split(' ')
+    assert (ss.Length >= 2)
+    let dir = ss[0]
+    let dist = Int32.Parse(ss[1])
+    dir, dist
+
+let parseInputIntoPoints (input: string[]) =
+    let rec inner inpIdx curPos =
+        seq {
+            if inpIdx < input.Length then
+                let line = input[inpIdx]
+                let (dir, dist) = parseLine line
+                let newPos = match dir with
+                                | "D" -> { curPos with Row = curPos.Row + dist }
+                                | "U" -> { curPos with Row = curPos.Row - dist }
+                                | "R" -> { curPos with Col = curPos.Col + dist }
+                                | "L" -> { curPos with Col = curPos.Col - dist }
+                                | a -> failwith $"Unecpected input '{a} {dist}' at input index {inpIdx}"
+                yield newPos
+                yield! (inner (inpIdx+1) newPos)
+        }
+    
+    // Input gives a closed loop, no need to yield the start
+    // as it will appear at the end
+    inner 0 { Row = 0; Col = 0 }
+    
+
 [<EntryPoint>]
 let main(args) =
     printfn $"Working folder: {Environment.CurrentDirectory}"
@@ -12,9 +42,12 @@ let main(args) =
     use diag = Utility.GetTracker ()
     let filename = args[0]
     printfn $"Input file {filename}"
-    let input = File.ReadLines(filename)
+    let input = File.ReadAllLines(filename)
     printfn ""
 
+    let points = parseInputIntoPoints input
+    for (idx, p) in (points |> Seq.mapi (fun idx p -> (idx, p))) do
+        printfn $"#{idx}: {p}"
 
     let result = -1
     printfn ""
