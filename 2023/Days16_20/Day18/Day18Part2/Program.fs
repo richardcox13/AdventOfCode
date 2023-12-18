fc81466e881f860868600c72d93482fb6754ed8b
Richard Cox <richard@rjcox.co.uk>
2023-12-18 15:28:01 +0000

    Day 17: part 2 is a fresh start for shoelace algorithm

---
+++ 2023/Days16_20/Day18/Day18Part2/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+open System.Collections.Generic
+open System.IO
+open AoC.Common
+open AoC.Common.Core
+
+[<EntryPoint>]
+let main(args) =
+    printfn $"Working folder: {Environment.CurrentDirectory}"
+    printfn $"Day 18 Part 2"
+    printfn ""
+    use diag = Utility.GetTracker ()
+    let filename = args[0]
+    printfn $"Input file {filename}"
+    let input = File.ReadLines(filename)
+    printfn ""
+
+
+    let result = -1
+    printfn ""
+    printfn $"Result = {result:``#,0``} ({result})"
+    printfn ""
+    0
