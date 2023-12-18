4209db5c13f11182e7d53582501129e5a12e4b20
Richard Cox <richard@rjcox.co.uk>
2023-12-18 10:09:33 +0000

    D17Pt1: find the size of the grid needed

--- 2023/Days16_20/Day18/Day18Part1/Program.fs
+++ 2023/Days16_20/Day18/Day18Part1/Program.fs
@@ -1,6 +1,42 @@
 ﻿open System
 open System.IO
 open AoC.Common
+open AoC.Common.Core
+
+type GridLimits = {
+    MinRow: int
+    MaxRow: int
+    MinCol: int
+    MaxCol: int
+}
+
+// Return (rows, cols)
+let findGridSize (inp: string seq) =
+    let input = inp |> Seq.toArray
+    let rec inner inputIdx (curPos: Position) (limits: GridLimits) =
+        if inputIdx >= input.Length then
+            limits
+        else
+            let ss = input[inputIdx].Split(' ')
+            assert (ss.Length >= 2)
+            let dir = ss[0]
+            let dist = Int32.Parse(ss[1])
+            let newPos = match dir with
+                             | "D" -> { curPos with Row = curPos.Row + dist }
+                             | "U" -> { curPos with Row = curPos.Row - dist }
+                             | "R" -> { curPos with Col = curPos.Col + dist }
+                             | "L" -> { curPos with Col = curPos.Col - dist }
+                             | a -> failwith $"Unecpected input '{a} {dist}' at input index {inputIdx}"
+            let newLimits = { MinRow = min newPos.Row limits.MinRow;
+                                        MaxRow = max newPos.Row limits.MaxRow;
+                                        MinCol = min newPos.Col limits.MinCol;
+                                        MaxCol = max newPos.Col limits.MaxCol }
+            inner (inputIdx+1) newPos newLimits
+
+    let maxLimits = inner 0 { Row=0; Col=0 } { MinRow=0; MaxRow=0; MinCol=0; MaxCol=0 }
+    let rows = maxLimits.MaxRow - maxLimits.MinRow + 1
+    let cols = maxLimits.MaxCol - maxLimits.MinCol + 1
+    (rows, cols)
 
 [<EntryPoint>]
 let main(args) =
@@ -12,6 +48,9 @@ let main(args) =
     printfn $"Input file {filename}"
     let input = File.ReadLines(filename)
     printfn ""
+    let (gridRows, gridCols) = findGridSize input
+    printfn $"The grid needs to be {gridRows} x {gridCols}"
+
 
     let result = -1
     printfn ""
