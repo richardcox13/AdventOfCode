3a14f83bf9cd938dc10c747a0e900ab1093357dc
Richard Cox <richard@rjcox.co.uk>
2023-12-18 17:00:49 +0000

    D17Pt2: shoelace fix 2 correct (add half the toal boundary) Part 1 Input

--- 2023/Days16_20/Day18/Day18Part2/Program.fs
+++ 2023/Days16_20/Day18/Day18Part2/Program.fs
@@ -13,22 +13,26 @@ let parseLine (line : string)=
     let dist = Int32.Parse(ss[1])
     dir, dist
 
-let parseInputIntoPoints (input: string[]) =
-    let rec inner inpIdx curPos =
+// Returns (dir * dist) seq
+let getInstructions (input: string seq) =
+    input
+      |> Seq.map (fun line-> parseLine line)
+
+let parseInputIntoPoints (instrctions: (string * int)[]) =
+    let rec inner instrIdx curPos =
         seq {
-            if inpIdx < input.Length then
-                let line = input[inpIdx]
-                let (dir, dist) = parseLine line
+            if instrIdx < instrctions.Length then
+                let (dir, dist) = instrctions[instrIdx]
                 let newPos = match dir with
                                 // Lines are one thick, so make adjustment to enclose on
                                 // right and below
-                                | "D" -> { curPos with Row = curPos.Row + dist + 1 }
-                                | "U" -> { curPos with Row = curPos.Row - dist - 1 }
-                                | "R" -> { curPos with Col = curPos.Col + dist + 1 }
-                                | "L" -> { curPos with Col = curPos.Col - dist - 1 }
-                                | a -> failwith $"Unecpected input '{a} {dist}' at input index {inpIdx}"
+                                | "D" -> { curPos with Row = curPos.Row + dist }
+                                | "U" -> { curPos with Row = curPos.Row - dist }
+                                | "R" -> { curPos with Col = curPos.Col + dist }
+                                | "L" -> { curPos with Col = curPos.Col - dist }
+                                | a -> failwith $"Unecpected input '{a} {dist}' at input index {instrIdx}"
                 yield newPos
-                yield! (inner (inpIdx+1) newPos)
+                yield! (inner (instrIdx+1) newPos)
         }
     
     // Input gives a closed loop, no need to yield the start
@@ -58,19 +62,27 @@ let main(args) =
     use diag = Utility.GetTracker ()
     let filename = args[0]
     printfn $"Input file {filename}"
-    //let input = File.ReadAllLines(filename)
-    let input = [| "R 3"; "D 3"; "L 3"; "U 3" |]
-    let input = [| "R 5"; "D 3"; "L 2"; "D 2"; "L 3"; "U 5" |]
+    let input = File.ReadAllLines(filename)
+    //let input = [| "R 3"; "D 3"; "L 3"; "U 3" |]
+    //let input = [| "R 5"; "D 3"; "L 2"; "D 2"; "L 3"; "U 5" |]
     printfn ""
 
+    let instructions = getInstructions input |> Seq.toArray
+
     let points
-        = parseInputIntoPoints input
+        = parseInputIntoPoints instructions
           |> Seq.toArray
 
-    for (idx, p) in (points |> Seq.mapi (fun idx p -> (idx, p))) do
-        printfn $"#{idx}: {p}"
+    //for (idx, p) in (points |> Seq.mapi (fun idx p -> (idx, p))) do
+    //    printfn $"#{idx}: {p}"
 
-    let result = shoelace points
+    let shoelaceArea = shoelace points
+    let boundaryArea
+        = instructions
+          |> Seq.map (fun (_, dist) -> dist)
+          |> Seq.sum
+
+    let result = int ((float shoelaceArea) + (float boundaryArea)/2.0 + 1.0)
     printfn ""
     printfn $"Result = {result:``#,0``} ({result})"
     printfn ""
