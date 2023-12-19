44db8eddcda3ee918c0ee8e844067060453aa15a
Richard Cox <richard@rjcox.co.uk>
2023-12-19 16:35:06 +0000

    D17Pt2: strip out part parsing, loading, & processing

--- 2023/Days16_20/Day19/Day19Part2/Program.fs
+++ 2023/Days16_20/Day19/Day19Part2/Program.fs
@@ -4,13 +4,6 @@ open System.IO
 open System.Text.RegularExpressions
 open AoC.Common
 
-type Part = {
-        A: int
-        M: int
-        S: int
-        X: int
-    }
-
 type Rule =
     | Goto of name:string
     | GreaterThan of Attribute:string * Value:int * Next:string
@@ -52,75 +45,19 @@ let parseWorkflow (line: string) =
 
     { Name = name; Rules = rules }
 
-// Eg, "{x=2127,m=1623,a=2188,s=1013}"
-let parsePart (line: string) =
-    let m = Regex.Match(line, @"{x=(?<x>\d+),m=(?<m>\d+),a=(?<a>\d+),s=(?<s>\d+)")
-    assert (m.Success)
-
-    let g (n:string) = Int32.Parse(m.Groups[n].Value)
-
-    { A = g "a"; M = g "m"; S = g "s"; X = g "x" }
-
-// Returns (Workdlow[] * Part[])
+// Returns Workdlow[]
 let parseInput (input: string[]) =
     let mutable loadParts = false
 
     let workflows = new List<Workflow>()
-    let parts = new List<Part>()
-    for line in input do
-        match (loadParts, line) with
-        | (false, "") ->
+    for line in (input |> Seq.where (fun _ -> not loadParts)) do
+        match line with
+        | "" ->
             loadParts <- true
-        | (false, l) ->
+        | l ->
             workflows.Add(parseWorkflow l)
-        | (true, l) ->
-            parts.Add(parsePart l)
 
-    (workflows |> Seq.toArray), (parts |> Seq.toArray)
-
-let processPart (workflows: Map<string, Workflow>) (part: Part) =
-    let getAttr attr =
-        match attr with
-        | "a" -> part.A
-        | "s" -> part.S
-        | "m" -> part.M
-        | "x" -> part.X
-        | x -> failwith $"Invalid attribute \"{x}\""
-
-    // return true for accepted and false for rejected
-    let rec runWorkflows wfName =
-        let rec runRules ruleIdx (rules: Rule[]) =
-            assert (ruleIdx < rules.Length)
-            let r = rules[ruleIdx]
-            match r with
-            | Goto(n) -> n
-            | GreaterThan(attr,value,name) ->
-                let a = getAttr attr
-                if a > value then
-                    name
-                else
-                    runRules (ruleIdx+1) rules
-            | LessThan(attr,value,name) ->
-                let a = getAttr attr
-                if a < value then
-                    name
-                else
-                    runRules (ruleIdx+1) rules
-
-        printf $"{wfName} -> "
-        let wf = workflows[wfName]
-        let next = runRules 0 wf.Rules
-        match next with
-         | "A" ->
-            let res = part.A + part.M + part.S + part.X
-            printfn $"accept ({res})"
-            res
-         | "R" ->
-            printfn "REJECT"
-            0
-         | x -> runWorkflows x
-
-    runWorkflows "in"
+    (workflows |> Seq.toArray)
 
 [<EntryPoint>]
 let main(args) =
@@ -133,16 +70,13 @@ let main(args) =
     let input = File.ReadAllLines(filename)
     printfn ""
 
-    let (workflowArray, parts) = parseInput input
-    printfn $"There are {workflowArray.Length} workflows, and {parts.Length} parts"
+    let workflowArray = parseInput input
+    printfn $"There are {workflowArray.Length} workflows"
 
     let workflows = Map (workflowArray |> Seq.map (fun w -> w.Name, w))
 
 
-    let result
-        = parts
-          |> Seq.map(fun p -> processPart workflows p)
-          |> Seq.sum
+    let result = -1
     printfn ""
     printfn $"Result = {result:``#,0``} ({result})"
     printfn ""
