df7c82b9543e6fc6337fe3876a5a16fb4b9a46f7
Richard Cox <richard@rjcox.co.uk>
2023-12-19 09:01:28 +0000

    Day 17: part 1 project created

---
+++ 2023/Days16_20/Day19/Day19Part1/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+open System.Collections.Generic
+open System.IO
+open AoC.Common
+
+[<EntryPoint>]
+let main(args) =
+    printfn $"Working folder: {Environment.CurrentDirectory}"
+    printfn $"Day 19 Part 1"
+    printfn ""
+    use diag = Utility.GetTracker ()
+    let filename = args[0]
+    printfn $"Input file {filename}"
+    let input = File.ReadAllLines(filename)
+    printfn ""
+
+    let result = -1
+    printfn ""
+    printfn $"Result = {result:``#,0``} ({result})"
+    printfn ""
+    0
