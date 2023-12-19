b2435b2bd26dc4cca452e4aada99b29fab118fab
Richard Cox <richard@rjcox.co.uk>
2023-12-19 12:07:17 +0000

    D17Pt1: dfata structures and outline of input parsing

--- 2023/Days16_20/Day19/Day19Part1/Program.fs
+++ 2023/Days16_20/Day19/Day19Part1/Program.fs
@@ -1,7 +1,54 @@
 ﻿open System
 open System.Collections.Generic
 open System.IO
+open System.Text.RegularExpressions
 open AoC.Common
+
+type Part = {
+        A: int
+        M: int
+        S: int
+        X: int
+    }
+
+type Rule =
+    | Goto of name:string
+    | GreaterTHan of Attribute:string * Value:int * Next:string
+    | LessTHan of Attribute:string * Value:int * Next:string
+
+type Workflow = {
+        Name: string
+        Rules: Rule[]
+    }
+
+// eg, "ex{x>10:one,m<20:two,a>30:R,A}"
+let parseWorkflow (line: string) =
+    let m = Regex.Match(line, @"(?<name>\p{Ll}+){(?<rules>.*)}")
+    assert (m.Success)
+    let name = m.Groups["name"].Value
+    let rules = m.Groups["rules"].Value
+
+    { Name = name; Rules = Array.empty<Rule> }
+
+let parsePart (line: string) =
+    { A = -1; M = -1; S = -1; X = -1}
+
+// Returns (Workdlow[] * Part[])
+let parseInput (input: string[]) =
+    let mutable loadParts = false
+
+    let workflows = new List<Workflow>()
+    let parts = new List<Part>()
+    for line in input do
+        match (loadParts, line) with
+        | (false, "") ->
+            loadParts <- true
+        | (false, l) ->
+            workflows.Add(parseWorkflow l)
+        | (true, l) ->
+            parts.Add(parsePart l)
+
+    (workflows |> Seq.toArray), (parts |> Seq.toArray)
 
 [<EntryPoint>]
 let main(args) =
@@ -13,6 +60,10 @@ let main(args) =
     printfn $"Input file {filename}"
     let input = File.ReadAllLines(filename)
     printfn ""
+
+    let (workflowArray, parts) = parseInput input
+
+    printfn $"There are {workflowArray.Length} workflows, and {parts.Length} parts"
 
     let result = -1
     printfn ""
