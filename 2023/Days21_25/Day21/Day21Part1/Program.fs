7188a3aaa8dc77ca0cc155885dfc740a1d14bf81
Richard Cox <richard@rjcox.co.uk>
2023-12-21 17:29:21 +0000

    D21Pt1: KISS approach works for test data

--- 2023/Days21_25/Day21/Day21Part1/Program.fs
+++ 2023/Days21_25/Day21/Day21Part1/Program.fs
@@ -5,7 +5,6 @@ open System.Text.RegularExpressions
 open AoC.Common
 open AoC.Common.Core
 
-
 let findChar (target: char) (grid: string[]) =
     let (row, col)
         = seq { 0 .. (grid.Length-1) }
@@ -14,6 +13,23 @@ let findChar (target: char) (grid: string[]) =
           |> Seq.exactlyOne
     { Row = row; Col = col }
 
+let takeSteps (grid: string[]) start stepCount =
+    let rec inner remaining pos =
+        seq {
+            if pos.Row >= 0
+                    && pos.Row < grid.Length
+                    && pos.Col >= 0
+                    && pos.Col < grid[0].Length
+                    && grid[pos.Row][pos.Col] <> '#' then
+                if remaining = 0 then
+                    yield pos
+                else
+                    yield! (inner (remaining-1) (Pos.moveUp pos))
+                    yield! (inner (remaining-1) (Pos.moveDown pos))
+                    yield! (inner (remaining-1) (Pos.moveLeft pos))
+                    yield! (inner (remaining-1) (Pos.moveRight pos))
+        }
+    inner stepCount start |> Seq.distinct
 
 [<EntryPoint>]
 let main(args) =
@@ -29,6 +45,9 @@ let main(args) =
     let start = findChar 'S' input
     printfn $"Starting at {start}"
 
+    let steps = takeSteps input start 6
+    for (idx,s) in (steps |> Seq.mapi (fun idx p -> idx,p)) do
+        printfn $"#{idx+1} Reached {s}"
 
     let result = -1
     printfn ""
