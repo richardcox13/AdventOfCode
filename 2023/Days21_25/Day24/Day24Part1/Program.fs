eb4245b46095da28d18f17808b9de7c7395799f4
Richard Cox <richard@rjcox.co.uk>
2023-12-29 16:43:59 +0000

    D24Pt1: calc crossing points... but incorrect

--- 2023/Days21_25/Day24/Day24Part1/Program.fs
+++ 2023/Days21_25/Day24/Day24Part1/Program.fs
@@ -37,6 +37,39 @@ let allPairs<'T> (collection: 'T array) =
                 yield (collection[first], collection[second])
     }
 
+// Returns double option: if None no crossing otherwise the time.
+let getTimeOfCrossing left right =
+    let xDenom = right.VelX - left.VelX
+    let xNum = left.StartX - right.StartY
+    if xDenom <> 0 && xNum <> 0 then
+        Some (xNum / xDenom)
+    else
+        let yDenom = right.VelY - left.VelY
+        let yNum = left.StartY - right.StartX
+        if yDenom <> 0 && yNum <> 0 then
+            Some (yNum / yDenom)
+        else
+            None
+
+let getCrossing left right =
+    let tt = getTimeOfCrossing left right
+    if tt.IsNone then
+        None
+    else
+        let t = tt.Value
+        if t > 0 then
+            let x = left.StartX + t * left.VelX
+            let y = left.StartY + t * left.VelY
+
+            let altX =  right.StartX + t * right.VelX
+            assert ((x - altX) < (Double.Epsilon * x))
+            let altY = right.StartY + t * right.VelY
+            assert (((y - altY)) < (Double.Epsilon * y))
+
+            Some (x, y)
+        else
+            None
+
 [<EntryPoint>]
 let main(args) =
     printfn $"Working folder: {Environment.CurrentDirectory}"
@@ -46,12 +79,25 @@ let main(args) =
     let filename = args[0]
     printfn $"Input file {filename}"
     let input = File.ReadAllLines(filename)
+    let lowCoord = Double.Parse(args[1])
+    let highCoord = Double.Parse(args[2])
+    printfn $"Looking for crossings in [{lowCoord}, {highCoord}]"
     printfn ""
 
     let lines = input |> parseInput |> Seq.toArray
 
-    for (f, s) in (lines |> allPairs) do
-        printfn $"({f.Index}, {s.Index})"
+
+    let count
+        = lines
+          |> allPairs
+          |> Seq.map (fun (left, right) -> left, right, (getCrossing left right))
+          |> Seq.iter (fun (left, right, crossing) ->
+                if crossing.IsNone then
+                    printfn $"Lines {left.Index} and {right.Index}: do not cross"
+                else
+                    let c = crossing.Value
+                    printfn $"Lines {left.Index} and {right.Index}: cross at ({fst c}, {snd c})"
+            )
 
     let result = -1
     printfn ""
